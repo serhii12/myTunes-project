6e19717f3f5a77d0cb8b5dad1eca8d10574ac470
Serhii Panchyshyn <panchyshyn.serhii@gmail.com>
2018-09-23 21:35:55 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Smooth Scrolling
+$('.main-header__nav a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
---
+++ readme.md
@@ -0,0 +1,4 @@
+# myTunes
+
+A fully-responsive and web accessible copy of Itunes design. Using CSS Grid and Flexbox. 
+
